f9557fd359f7b3c0c063533a6ad3af7dc916c5ba
Eric <eric.f.meyer@gmail.com>
2018-05-22 17:49:04 -0500

    Dice: modify game to roll until a double is rolled

--- RandomNumbers/Dice.java
+++ RandomNumbers/Dice.java
@@ -3,14 +3,18 @@ import java.util.Random;
 public class Dice {
     public static void main(String[] args) {
         Random r = new Random();
-        int dice1, dice2;
+        int dice1 = 1, dice2 = 0;
+        int rolls = 0;
 
-        dice1 = (1 + r.nextInt(6));
-        dice2 = (1 + r.nextInt(6));
-
-        System.out.println("HERE COMES THE DICE!\n");
-        System.out.println("Roll #1: " + dice1);
-        System.out.println("Roll #2: " + dice2);
-        System.out.println("The total is " + (dice1 + dice2) + "!");
+        System.out.println("HERE COMES THE DICE!");
+        while (dice1 != dice2) {
+            dice1 = (1 + r.nextInt(6));
+            dice2 = (1 + r.nextInt(6));
+            System.out.println("\nRoll #1: " + dice1);
+            System.out.println("Roll #2: " + dice2);
+            System.out.println("The total is " + (dice1 + dice2) + "!");
+            rolls++;
+        }
+        System.out.println("\nIt took " + rolls + " rolls to get a double!");
     }
 }
