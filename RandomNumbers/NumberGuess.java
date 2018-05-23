0017aa39e7aa2ef9a2dad73f6cd4f629edfda5b5
Eric Meyer <MeyerEricF@JohnDeere.com>
2018-05-23 10:18:53 -0500

    NumberGuess: output number of tries

--- RandomNumbers/NumberGuess.java
+++ RandomNumbers/NumberGuess.java
@@ -6,19 +6,20 @@ public class NumberGuess {
         Random r = new Random();
         Scanner keyboard = new Scanner(System.in);
         int secret = 1 + r.nextInt(10);
-        int number, guesses = 0;
+        int guess, tries = 1;
 
         System.out.println("I'm thinking of a number from 1 to 10. Try to guess it.");
         System.out.print("Your guess: ");
-        number = keyboard.nextInt();
+        guess = keyboard.nextInt();
 
-        while (number != secret) {
-            guesses++;
+        while (guess != secret) {
             System.out.println("That is incorrect. Guess again.");
             System.out.print("Your guess: ");
-            number = keyboard.nextInt();
+            guess = keyboard.nextInt();
+            tries++;
         }
 
         System.out.println("That's right! My secret number was " + secret + "!");
+        System.out.println("It took you " + tries + " tries to guess the number.");
     }
 }
