5211c3f533f8de4014a25784435561bc0160212e
Eric Meyer <MeyerEricF@JohnDeere.com>
2018-05-23 10:42:01 -0500

    Dice: replace while loop with a do-while loop

--- RandomNumbers/Dice.java
+++ RandomNumbers/Dice.java
@@ -3,18 +3,20 @@ import java.util.Random;
 public class Dice {
     public static void main(String[] args) {
         Random r = new Random();
-        int dice1 = 1, dice2 = 0;
+        int dice1, dice2;
         int rolls = 0;
 
         System.out.println("HERE COMES THE DICE!");
-        while (dice1 != dice2) {
+
+        do {
             dice1 = (1 + r.nextInt(6));
             dice2 = (1 + r.nextInt(6));
+            rolls++;
             System.out.println("\nRoll #1: " + dice1);
             System.out.println("Roll #2: " + dice2);
             System.out.println("The total is " + (dice1 + dice2) + "!");
-            rolls++;
-        }
+        } while (dice1 != dice2);
+
         System.out.println("\nIt took " + rolls + " rolls to get a double!");
     }
 }
