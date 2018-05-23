f204f7c1dee661c00dc3f954733377110e2a9e7f
Eric Meyer <MeyerEricF@JohnDeere.com>
2018-05-23 11:25:20 -0500

    NumberGuess: replace while loop with do-while loop

--- RandomNumbers/NumberGuess.java
+++ RandomNumbers/NumberGuess.java
@@ -6,18 +6,18 @@ public class NumberGuess {
         Random r = new Random();
         Scanner keyboard = new Scanner(System.in);
         int secret = 1 + r.nextInt(10);
-        int guess, tries = 1;
+        int guess, tries = 0;
 
         System.out.println("I'm thinking of a number from 1 to 10. Try to guess it.");
-        System.out.print("Your guess: ");
-        guess = keyboard.nextInt();
 
-        while (guess != secret) {
-            System.out.println("That is incorrect. Guess again.");
+        do {
             System.out.print("Your guess: ");
             guess = keyboard.nextInt();
             tries++;
-        }
+            if (guess != secret) {
+                System.out.println("That is incorrect. Guess again.");
+            }
+        } while (guess != secret);
 
         System.out.println("That's right! My secret number was " + secret + "!");
         System.out.println("It took you " + tries + " tries to guess the number.");
